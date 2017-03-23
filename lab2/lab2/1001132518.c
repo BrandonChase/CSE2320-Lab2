591a044f6350196964b5ccf70c90fff117f8a15e
Brandon Chase <brandonc2127@gmail.com>
2017-03-23 01:08:54 -0500

    final

--- lab2/lab2/1001132518.c
+++ lab2/lab2/1001132518.c
@@ -1,12 +1,12 @@
 /*
-Lab 2
-Brandon Chase
-ID: 1001132518
+	Lab 2
+	Brandon Chase
+	ID: 1001132518
 
-Description: This program takes a set of numbers and a target sum as input and uses dynamic programming to calculate all possible 
-cardinalities of the subset such that the elements add up to the target sum.
+	Description: This program takes a set of numbers and a target sum as input and uses dynamic programming to calculate all possible 
+	cardinalities of the subset such that the elements add up to the target sum.
 
-Compile Command: gcc lab2.c -o lab2.out
+	Compile Command: gcc 1001132518.c -o 1001132518.out
 */
 
 #include <stdlib.h>
@@ -79,14 +79,12 @@ void readInput(int* inputs_size, int* target_sum, int** inputs, int*** cost_tabl
 }
 
 /*
-@desc Determines whether or not the interleaved sequence that results from applying the given interleave factor to
-array X is a subsequence of array A
-@param int* x - pointer to first element of array X
-@param x_len - size of array X
-@param int* a - pointer to the first element of array A
-@param int a_len - size of the array A
-@param int interleave_factor - the interleave factor that is applied to array X
-@return (bool)int - is X^(interleave factor) a subsequence of A or not
+	@desc Uses dynamic programming to fill up a 2D cost table that stores the previously included value to reach the subset sum (row number) with the given cardinality (column number).
+	Builds itself based on the base case and then previous values for values later down the list.
+	@param int inputs_size - size of input array
+	@param int target_sum - the desired subset sum
+	@param int* inputs - pointer to the array that will store the series of numbers
+	@param int** cost_table - pointer to 2D array that will store the 2D cost table
 */
 void fillCostTable(int inputs_size, int target_sum, int* inputs, int** cost_table)
 {
@@ -123,19 +121,15 @@ void fillCostTable(int inputs_size, int target_sum, int* inputs, int** cost_tabl
 }
 
 /*
-@desc Determines whether or not the interleaved sequence that results from applying the given interleave factor to
-array X is a subsequence of array A
-@param int* x - pointer to first element of array X
-@param x_len - size of array X
-@param int* a - pointer to the first element of array A
-@param int a_len - size of the array A
-@param int interleave_factor - the interleave factor that is applied to array X
-@return (bool)int - is X^(interleave factor) a subsequence of A or not
+	@desc Prints out the target sum and the array of inputted numbers including subscripts
+	@param int inputs_size - size of input array
+	@param int target_sum - the desired subset sum
+	@param int* inputs - pointer to the array that will store the series of numbers
 */
 void printInput(int inputs_size, int target_sum, int* inputs)
 {
 	printf("m is %d\n", target_sum);
-	printf("  i   inputs\n");
+	printf("  i   S\n");
 	printf("-------\n");
 	int i;
 	for (i = 0; i <= inputs_size; i++)
@@ -145,14 +139,10 @@ void printInput(int inputs_size, int target_sum, int* inputs)
 }
 
 /*
-@desc Determines whether or not the interleaved sequence that results from applying the given interleave factor to
-array X is a subsequence of array A
-@param int* x - pointer to first element of array X
-@param x_len - size of array X
-@param int* a - pointer to the first element of array A
-@param int a_len - size of the array A
-@param int interleave_factor - the interleave factor that is applied to array X
-@return (bool)int - is X^(interleave factor) a subsequence of A or not
+	@desc Prints out the cost table
+	@param int inputs_size - size of input array
+	@param int target_sum - the desired subset sum
+	@param int** cost_table - pointer to 2D array that will store the 2D cost table
 */
 void printCostTable(int inputs_size, int target_size, int** cost_table)
 {
@@ -169,14 +159,11 @@ void printCostTable(int inputs_size, int target_size, int** cost_table)
 }
 
 /*
-@desc Determines whether or not the interleaved sequence that results from applying the given interleave factor to
-array X is a subsequence of array A
-@param int* x - pointer to first element of array X
-@param x_len - size of array X
-@param int* a - pointer to the first element of array A
-@param int a_len - size of the array A
-@param int interleave_factor - the interleave factor that is applied to array X
-@return (bool)int - is X^(interleave factor) a subsequence of A or not
+	@desc Print backtrace that includes the values that contribute to the subset sum for each cardinalities
+	@param int inputs_size - size of input array
+	@param int target_sum - the desired subset sum
+	@param int* inputs - pointer to the array that will store the series of numbers
+	@param int** cost_table - pointer to 2D array that will store the 2D cost table
 */
 void printBacktrace(int inputs_size, int target_sum, int* inputs, int** cost_table)
 {
@@ -185,12 +172,12 @@ void printBacktrace(int inputs_size, int target_sum, int* inputs, int** cost_tab
 	{
 		if (cost_table[target_sum][cardinality] == inputs_size + 1)
 		{
-			printf("No inputsolution with %2d elements\n", cardinality+1);
+			printf("No solution with %d elements\n", cardinality+1);
 		}
 		else
 		{
 			printf("Solution with %2d elements\n", cardinality + 1);
-			printf("  i   inputs\n");
+			printf("  i   S\n");
 			printf("-------\n");
 			int i = target_sum, temp_card = cardinality;
 			while (i > 0 && temp_card >= 0)
